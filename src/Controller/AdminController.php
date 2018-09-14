1b8e920322b40254cdeea80c906ed99d6d45bf58
Adrien Thierry Lafage <adrien.thierry.lafage@eisti.fr>
2018-09-15 01:50:21 +0200

    Administrator space security raised

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class AdminController extends Controller
 {
@@ -12,8 +17,12 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')){
+    		throw new AccessDeniedException('Accès limité aux administrateurs.');
+    	}else{
+            return $this->render('admin/home.html.twig', [
+                'controller_name' => 'AdminController',
+            ]);
+        }
     }
 }
